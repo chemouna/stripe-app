c8cf0be3a702a43c7e5035e177bdd28205abaf9d
Mouna Cheikhna <cheikhnamouna@gmail.com>
2016-09-15 07:11:00 +0200

    Make stripe config work.

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,34 @@
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-import Lib
+import           Data.ByteString (ByteString)
+import qualified Data.ByteString.Char8 as B8
+import           Lib
+import           Web.Stripe
+import           Web.Stripe.Customer
+import           Data.Text             (Text)
+import qualified Data.Text             as T
+import Web.Stripe.Account
+
+-- import qualified Data.Text.Encoding    as T
 
 main :: IO ()
-main = someFunc
+main = do
+  let config = StripeConfig (StripeKey (toBytestring "mykey"))
+  print "Test"
+
+
+-- packEnv :: String -> IO B.ByteString
+-- packEnv = fmap B.pack . getEnv
+
+toBytestring :: Show a => a -> ByteString
+toBytestring = B8.pack . show
+
+toText
+    :: Show a
+    => a
+    -> Text
+toText = T.pack . show
+
